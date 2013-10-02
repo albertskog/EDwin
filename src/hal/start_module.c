1c2cb2aa03077681d2c62500b7a9fcaade358dc4
Jacob Pålsson <jacpa975@student.liu.se>
2013-10-02 18:03:18 +0200

    Update start_module.c

---
+++ src/hal/start_module.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+** Function name:		startModuleInit
+**
+** Descriptions:		Initializes the start module
+**
+** parameters:			None
+** Returned value:		None
+** 
+******************************************************************************/
+uint8 startModule_Init(){}
+
+/******************************************************************************
+** Function name:		receivedStartSignal
+**
+** Descriptions:		Polls start-pin on the start module
+**
+** parameters:			None
+** Returned value:		STARTMODULE_START
+** 
+******************************************************************************/
+uint8 startModule_receivedStartSignal(){}
