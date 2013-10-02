a342752680a93f84d5d27e6d63ca918bd744b8b2
Jacob Pålsson <jacpa975@student.liu.se>
2013-10-02 17:51:45 +0200

    Update motor.c
    Added the setMotor function

---
+++ src/hal/motor.c
@@ -0,0 +1,13 @@
+/******************************************************************************
+** Function name:		setMotor
+**
+** Descriptions:		Sets the speed and direction for specified motor pair
+**
+** parameters:
+*             motor     - ex: MOTOR_RIGHT / MOTOR_LEFT
+*             direction - ex: MOTOR_FORWARD / MOTOR_BACKWARDS
+*             speed     - Speed of the specified motor pair in percentage
+** Returned value:		None
+** 
+******************************************************************************/
+void setMotor(uint8 motor, uint8 direction, uint8 speed){}
